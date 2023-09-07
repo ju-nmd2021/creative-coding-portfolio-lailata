39091a130d67f6bb59acdc2cd1e22a94163d9b2a
lailata <93602827+lailata@users.noreply.github.com>
2023-09-07 16:57:37 +0200

    Flow Field Variations
    Added some more Flow Fields

--- experiments/flowField2.js
+++ experiments/flowField2.js
@@ -1,5 +1,5 @@
 let particles = [];
-const num = 5000;
+const num = 1000;
 
 const noiseScale = 0.01;
 
@@ -9,24 +9,21 @@ function setup() {
     particles.push(createVector(random(width), random(height)));
   }
   background(34, 39, 46);
-  stroke(0, 255, 255);
+  stroke(270, 100, 255);
   strokeWeight(1.5);
   angleMode(DEGREES);
 }
 
-/* Drwaing points on random x and y positions, 
-finding out the value of a point and adding noiseScale
-*/
 function draw() {
-  background(0, 10);
+  background(0, 5);
   for (let i = 0; i < num; i++) {
     let position = particles[i];
     point(position.x, position.y);
 
     let pointValue = (position.x * noiseScale, position.y * noiseScale);
-    let angle = 360 * pointValue;
+    let angle = 15 * pointValue;
     position.x += cos(angle);
-    position.y += sin(angle);
+    position.y -= sin(angle);
 
     if (!onScreen(position)) {
       position.x = random(width);
@@ -37,8 +34,4 @@ function draw() {
 
 function onScreen(v) {
   return v.x >= 0 && v.x <= width && v.y >= 0 && v.y <= height;
-}
-
-function mousePressed() {
-  saveCanvas("flowfield", "png");
 }
---
+++ experiments/flowField3.js
@@ -0,0 +1,74 @@
+let points = [];
+let angleControl;
+
+let red1;
+let red2;
+let blue1;
+let blue2;
+let green1;
+let green2;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(30);
+  noiseDetail(1);
+  angleMode(DEGREES);
+
+  let density = 60;
+  let space = width / density;
+
+  for (var x = 0; x < width; x += space) {
+    for (var y = 0; y < height; y += space) {
+      let p = createVector(x + random(-20, 10), y + random(-30, +5));
+      points.push(p);
+    }
+  }
+
+  red1 = random(255);
+  red2 = random(255);
+  green1 = random(255);
+  green2 = random(255);
+  blue1 = random(255);
+  blue2 = random(255);
+
+  angleControl = random(0.002, 0.05);
+}
+
+function draw() {
+  noStroke();
+
+  for (var i = 0; i < points.length; i++) {
+    let red = map(points[i].x, 0, width, red1, red2);
+    let green = map(points[i].y, 0, height, green1, green2);
+    let blue = map(points[i].x, 0, width, blue1, blue2);
+
+    fill(red, green, blue);
+
+    let angle = map(
+      noise(points[i].x * angleControl, points[i].y * angleControl),
+      0,
+      1,
+      0,
+      720
+    );
+
+    points[i].add(createVector(cos(angle), sin(angle)));
+
+    let sizeEllipse = random(100);
+    if (dist(width / 2, height / 2, points[i].x, points[i].y) < sizeEllipse) {
+      ellipse(points[i].x, points[i].y, 1);
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
